e13d3c50b706c20eaf42327df484a899abb0b2e1
rayanfox <35785332+rayanfox@users.noreply.github.com>
2023-07-26 18:06:51 -0500

    Updated bounce.py to work correctly

--- Code_Files/python/Intro to python code assignments/Chapter 3/bounce.py
+++ Code_Files/python/Intro to python code assignments/Chapter 3/bounce.py
@@ -1,12 +1,13 @@
-initial_height = float(input("Enter the height from which the ball is dropped: "))
-bounciness_index = float(input("Enter the bounciness index of the ball: "))
-num_bounces = int(input("Enter the number of times the ball is allowed to continue bouncing: "))
+height = int(input("Enter the height from which the ball is dropped: "))
+bounceIndex = float(input("Enter the bounciness index of the ball: "))
+numberOfBounces = int(input("Enter the number of times the ball is allowed to continue bouncing: "))
 
-# Calculate and print the total distance
-total_distance = initial_height
-for _ in range(num_bounces):
-    bounce_height = bounciness_index * total_distance
-    total_distance += 2 * bounce_height
+distance = 0
 
-rounded_distance = round(total_distance, 3)
-print("Total distance traveled is: {} units.".format(rounded_distance))
+while numberOfBounces > 0:
+    distance = distance + height
+    height = height * bounceIndex
+    distance = distance + height
+    numberOfBounces -= 1
+
+print("Total distance traveled is:", distance, "units.")
