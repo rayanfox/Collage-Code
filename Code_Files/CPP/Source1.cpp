7ec87617d6fc75f595fb90ded25dea0c65ae7b19
rayanfox <35785332+rayanfox@users.noreply.github.com>
2023-08-23 09:58:47 -0500

    Create Source1.cpp

---
+++ Code_Files/CPP/Source1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+
+{
+
+	cout << "hello world\n";
+	cout << "whats your name? >";
+	string name;
+	cin >> name;
+	cout << "hello, " << name << "!" << endl;
+
+	return 0;
+}
